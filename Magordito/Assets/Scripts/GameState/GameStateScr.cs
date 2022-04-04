11c28f1b4059ae8a02b2598a7deaea40e09d5744
Adrián Camacho <adricamachogarcia@gmail.com>
2022-04-04 15:48:18 +0200

    Nuevas escenas

--- Magordito/Assets/Scripts/GameState/GameStateScr.cs
+++ Magordito/Assets/Scripts/GameState/GameStateScr.cs
@@ -7,12 +7,18 @@ public class GameStateScr : MonoBehaviour
     public int nPlayers;
     public int[] vidasPlayers;
     public int[] puntosPlayers;
+
+    [Header("Constantes")]
+    public int vidasMax;
     private void Start()
     {
         vidasPlayers = new int[nPlayers];
-       
         puntosPlayers = new int[nPlayers];
-        
-    }
 
+        for (int i = 0; i < nPlayers; i++)
+        {
+            vidasPlayers[i] = vidasMax;
+            puntosPlayers[i] = 0;
+        }
+    }
 }
