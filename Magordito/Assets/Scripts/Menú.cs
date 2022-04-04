56cb01dca29bbceeaf14144ffa607158c52590da
Gmonesq <gmonesq@gmail.com>
2022-04-04 10:04:11 +0200

    Unity
    Si

---
+++ Magordito/Assets/Scripts/Menú.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menú : MonoBehaviour
+{
+    private bool settingsen;
+    public GameObject settingsC;
+    public GameObject menu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void settings()
+    {
+        if(settingsen == true) {
+            menu.SetActive(true);
+            settingsC.SetActive(false);
+            settingsen = false;
+        }
+        else {
+            menu.SetActive(false);
+            settingsC.SetActive(true);
+            settingsen = true;
+        }
+
+    }
+    public void close()
+    {
+        Application.Quit();
+    }
+}
