2e9b86b955493450edb9e4054c550cab16ab6876
Adrián Camacho <adricamachogarcia@gmail.com>
2022-04-05 12:52:40 +0200

    SueloSueloSuelo

--- Magordito/Assets/Scripts/GameState/GameAssets.cs
+++ Magordito/Assets/Scripts/GameState/GameAssets.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameAssets : MonoBehaviour
 {
     [Header("Objetos Corruptos")]
-
+    public GameObject[] objetosCorruptos;
     public GameObject CalabazaC;
     public GameObject CalderoC;
     public GameObject MesaC;
@@ -13,14 +13,25 @@ public class GameAssets : MonoBehaviour
     public GameObject TelefonoC;
 
     [Header("Objetos Normales")]
-
+    public GameObject[] objetosNormales;
     public GameObject CalabazaN;
     public GameObject CalderoN;
     public GameObject MesaN;
     public GameObject PlantaN;
     public GameObject TelefonoN;
-    private void Start()
+    private void Awake()
     {
-        
+        objetosCorruptos = new GameObject[5];
+        objetosCorruptos[0] = CalabazaC;
+        objetosCorruptos[1] = CalderoC;
+        objetosCorruptos[2] = MesaC;
+        objetosCorruptos[3] = PlantaC;
+        objetosCorruptos[4] = TelefonoC;
+    objetosNormales = new GameObject[5];
+        objetosNormales[0] = CalabazaN;
+        objetosNormales[1] = CalderoN;
+        objetosNormales[2] = MesaN;
+        objetosNormales[3] = PlantaN;
+        objetosNormales[4] = TelefonoN;
     }
 }
--- Magordito/Assets/Scripts/GameState/Generation.cs
+++ Magordito/Assets/Scripts/GameState/Generation.cs
@@ -2,14 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Generation : MonoBehaviour
+public class Generation : MonoBehaviour 
 {
+    public int numObj;
+    private int numOC; //Numero de objetos corrompidos
+    
+    public Vector3[] objPositions;
+    public GameObject[] objetosCargados;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        numOC = gameObject.GetComponent<GameStateScr>().pointsToWin;
+        Generate();
     }
+
+    
+    void Generate() {
+        objetosCargados = new GameObject[objPositions.Length];
+        for (int i = 0; i < objPositions.Length; i++) {
+            
+            Instantiate(gameObject.GetComponent<GameAssets>().objetosNormales[Random.Range(0, 5)], objPositions[i], new Quaternion(0, 0, 0, 0));
+        }    
+    }
+    
+
+
 
 
 
