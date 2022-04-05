d746e5d51121c52d0e5c7cf72e699ef80243a195
Adrián Camacho <adricamachogarcia@gmail.com>
2022-04-05 10:16:15 +0200

    Generación 1

---
+++ Magordito/Assets/Scripts/GameState/GameAssets.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameAssets : MonoBehaviour
+{
+    [Header("ObjetosCorrompidos")]
+    public GameObject[] objetosCorrompidos;
+}
