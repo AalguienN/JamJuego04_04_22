d0670910bca6f35f203ce14d5b98f52060fd240f
Adrián Camacho <adricamachogarcia@gmail.com>
2022-04-05 15:39:44 +0200

    Fantasmas

--- Magordito/Assets/Scripts/Fantasma/fantasmaComportamiento.cs
+++ Magordito/Assets/Scripts/Fantasma/fantasmaComportamiento.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class fantasmaComportamiento : MonoBehaviour
 {
+    private Vector3 dir;
+    private GameObject player;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject,2);
+        player = GameObject.FindGameObjectWithTag("Player");
+        changeDir();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0.1f, 0);
+        if (Vector3.Distance(player.transform.position, this.transform.position) > 30) { changeDir(); }
+        if (transform.position.y < -2.5) { changeDir(); dir.y = Random.Range(1, 4); }
+        if (transform.position.y > 15) { changeDir();  dir.y = Random.Range(-3, 0); }
+        if (transform.position.z > 0) { dir.z = -1; }
+        if (transform.position.x > 0) { dir.x -= 1; }
+        if (transform.position.x < 0) { dir.x += 1; }
+        else if (dir.z < 0 && transform.position.z <= 0) { dir.z = 0; }
+        transform.Translate(dir * Time.deltaTime * speed);
+    }
+
+    private void changeDir() {
+        dir = new Vector3(Random.Range(-3, 4), Random.Range(-3, 4), 0);
     }
 }
